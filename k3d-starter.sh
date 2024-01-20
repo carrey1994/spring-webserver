2b2a5df429c470245ba3a67abcb495865e4286ae
James Wu <jameswu@JamesdeMacBook-Pro.local>
2024-01-20 22:15:31 +0800

    k8s yaml

---
+++ k3d-starter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+k3d cluster create --api-port 6550 -p "8081:80@loadbalancer" --agents 2
+
+docker build -t carrey1994/spring-webserver .
+
+YAML_FOLDER="kubernetes"
+
+# Check if the folder exists
+if [ ! -d "$YAML_FOLDER" ]; then
+  echo "Error: The specified folder does not exist."
+  exit 1
+fi
+
+# Loop through each YAML file in the folder
+for file in "$YAML_FOLDER"/*.yml; do
+  echo "Applying $file..."
+  kubectl apply -f "$file"
+  printf "\n"
+done
+
+echo "All YAML files applied successfully."
